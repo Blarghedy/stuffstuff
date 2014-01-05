1e0f7f07df7d91f98ef14070fa07105d65cccff1
Timothy McPherson <blarghedy@gmail.com>
2014-01-04 23:09:03 -0500

    Plaid stairs now use the proper textures

--- common/stuffstuff/blocks/BlockStuffStairs.java
+++ common/stuffstuff/blocks/BlockStuffStairs.java
@@ -1,22 +1,28 @@
 package stuffstuff.blocks;
 
-import stuffstuff.StuffStuff;
-import stuffstuff.info.BlockInfo;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStairs;
+import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
+import stuffstuff.StuffStuff;
+import stuffstuff.info.BlockInfo;
 
 public class BlockStuffStairs extends BlockStairs
 {
+	private Block model;
+
 	public BlockStuffStairs(int id, Block modelBlock)
 	{
 		super(id, modelBlock, 0);
+		model = modelBlock;
+
 		setLightOpacity(0);
 		setCreativeTab(StuffStuff.tabStuffStuff);
 		setHardness(modelBlock.blockHardness);
 		setResistance(modelBlock.blockResistance / 3.0F);
 		setStepSound(modelBlock.stepSound);
 		setBurnProperties(id, blockFireSpreadSpeed[modelBlock.blockID], blockFlammability[modelBlock.blockID]);
-		
+
 		Class cl = modelBlock.getClass();
 		if (cl.equals(BlockPlaidStone.class))
 		{
@@ -38,5 +44,11 @@ public class BlockStuffStairs extends BlockStairs
 		{
 			throw new RuntimeException("Attempted to make a StuffStairs of a non-stuffstuff material");
 		}
+	}
+
+	@Override
+	public Icon getBlockTexture(IBlockAccess world, int x, int y, int z, int par5)
+	{
+		return model.getBlockTexture(world, x, y, z, par5);
 	}
 }

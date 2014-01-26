4be44f2229e224f4b0dbfb444149c6fd8e36a3f3
Timothy McPherson <blarghedy@gmail.com>
2014-01-25 22:08:18 -0500

    Pre-large edit cleanup in StuffDoorRenderer

--- common/stuffstuff/render/StuffDoorRenderer.java
+++ common/stuffstuff/render/StuffDoorRenderer.java
@@ -83,8 +83,7 @@ public class StuffDoorRenderer implements ISimpleBlockRenderingHandler
 		tessellator.setBrightness(renderer.renderMinZ > 0.0D ? mixedBrightness : block.getMixedBrightnessForBlock(world, x, y, z - 1));
 		tessellator.setColorOpaque_F(zOpacity, zOpacity, zOpacity);
 
-		System.out.println(orientation + " " + isDoorOpen);
-		if (((orientation == 1 || orientation == 3) && !isDoorOpen))// || ((orientation == 0 || orientation == 2) && isDoorOpen))
+		if (((orientation == 1 || orientation == 3) && !isDoorOpen) || ((orientation == 0 || orientation == 2) && isDoorOpen))
 		{
 			renderFaceZNeg(block, x, y, z, icon, renderer);
 		}
@@ -137,71 +136,10 @@ public class StuffDoorRenderer implements ISimpleBlockRenderingHandler
 	{
 		Tessellator tessellator = Tessellator.instance;
 
-		if (renderer.hasOverrideBlockTexture())
-		{
-			icon = renderer.overrideBlockTexture;
-		}
-
 		double minU = icon.getInterpolatedU(renderer.renderMinX * 16.0D);
 		double maxU = icon.getInterpolatedU(renderer.renderMaxX * 16.0D);
 		double minV = icon.getInterpolatedV(16.0D - renderer.renderMaxY * 16.0D);
 		double maxV = icon.getInterpolatedV(16.0D - renderer.renderMinY * 16.0D);
-		double tmp;
-
-		if (renderer.renderMinX < 0.0D || renderer.renderMaxX > 1.0D)
-		{
-			minU = icon.getMinU();
-			maxU = icon.getMaxU();
-		}
-
-		if (renderer.renderMinY < 0.0D || renderer.renderMaxY > 1.0D)
-		{
-			minV = icon.getMinV();
-			maxV = icon.getMaxV();
-		}
-
-		tmp = maxU;
-		double U = minU;
-		double v = minV;
-		double V = maxV;
-
-		//		if (renderer.uvRotateEast == 2)
-		//		{
-		//			minU = icon.getInterpolatedU(renderer.renderMinY * 16.0D);
-		//			minV = icon.getInterpolatedV(16.0D - renderer.renderMinX * 16.0D);
-		//			maxU = icon.getInterpolatedU(renderer.renderMaxY * 16.0D);
-		//			maxV = icon.getInterpolatedV(16.0D - renderer.renderMaxX * 16.0D);
-		//			v = minV;
-		//			V = maxV;
-		//			tmp = minU;
-		//			U = maxU;
-		//			minV = maxV;
-		//			maxV = v;
-		//		}
-		//		else if (renderer.uvRotateEast == 1)
-		//		{
-		//			minU = icon.getInterpolatedU(16.0D - renderer.renderMaxY * 16.0D);
-		//			minV = icon.getInterpolatedV(renderer.renderMaxX * 16.0D);
-		//			maxU = icon.getInterpolatedU(16.0D - renderer.renderMinY * 16.0D);
-		//			maxV = icon.getInterpolatedV(renderer.renderMinX * 16.0D);
-		//			tmp = maxU;
-		//			U = minU;
-		//			minU = maxU;
-		//			maxU = U;
-		//			v = maxV;
-		//			V = minV;
-		//		}
-		//		else if (renderer.uvRotateEast == 3)
-		//		{
-		//			minU = icon.getInterpolatedU(16.0D - renderer.renderMinX * 16.0D);
-		//			maxU = icon.getInterpolatedU(16.0D - renderer.renderMaxX * 16.0D);
-		//			minV = icon.getInterpolatedV(renderer.renderMaxY * 16.0D);
-		//			maxV = icon.getInterpolatedV(renderer.renderMinY * 16.0D);
-		//			tmp = maxU;
-		//			U = minU;
-		//			v = minV;
-		//			V = maxV;
-		//		}
 
 		double xx = x + renderer.renderMinX;
 		double XX = x + renderer.renderMaxX;
@@ -213,22 +151,22 @@ public class StuffDoorRenderer implements ISimpleBlockRenderingHandler
 		{
 			tessellator.setColorOpaque_F(renderer.colorRedTopLeft, renderer.colorGreenTopLeft, renderer.colorBlueTopLeft);
 			tessellator.setBrightness(renderer.brightnessTopLeft);
-			tessellator.addVertexWithUV(xx, YY, zz, tmp, v);
+			tessellator.addVertexWithUV(xx, YY, zz, maxU, minV);
 			tessellator.setColorOpaque_F(renderer.colorRedBottomLeft, renderer.colorGreenBottomLeft, renderer.colorBlueBottomLeft);
 			tessellator.setBrightness(renderer.brightnessBottomLeft);
 			tessellator.addVertexWithUV(XX, YY, zz, minU, minV);
 			tessellator.setColorOpaque_F(renderer.colorRedBottomRight, renderer.colorGreenBottomRight, renderer.colorBlueBottomRight);
 			tessellator.setBrightness(renderer.brightnessBottomRight);
-			tessellator.addVertexWithUV(XX, yy, zz, U, V);
+			tessellator.addVertexWithUV(XX, yy, zz, minU, maxV);
 			tessellator.setColorOpaque_F(renderer.colorRedTopRight, renderer.colorGreenTopRight, renderer.colorBlueTopRight);
 			tessellator.setBrightness(renderer.brightnessTopRight);
 			tessellator.addVertexWithUV(xx, yy, zz, maxU, maxV);
 		}
 		else
 		{
-			tessellator.addVertexWithUV(xx, YY, zz, tmp, v);
+			tessellator.addVertexWithUV(xx, YY, zz, maxU, minV);
 			tessellator.addVertexWithUV(XX, YY, zz, minU, minV);
-			tessellator.addVertexWithUV(XX, yy, zz, U, V);
+			tessellator.addVertexWithUV(XX, yy, zz, minU, maxV);
 			tessellator.addVertexWithUV(xx, yy, zz, maxU, maxV);
 		}
 	}
@@ -237,78 +175,10 @@ public class StuffDoorRenderer implements ISimpleBlockRenderingHandler
 	{
 		Tessellator tessellator = Tessellator.instance;
 
-		if (renderer.hasOverrideBlockTexture())
-		{
-			icon = renderer.overrideBlockTexture;
-		}
-
 		double minU = icon.getInterpolatedU(renderer.renderMinX * 16.0D);
 		double maxU = icon.getInterpolatedU(renderer.renderMaxX * 16.0D);
 		double maxV = icon.getInterpolatedV(16.0D - renderer.renderMaxY * 16.0D);
 		double minV = icon.getInterpolatedV(16.0D - renderer.renderMinY * 16.0D);
-		double tmp;
-
-		if (renderer.flipTexture)
-		{
-			tmp = minU;
-			minU = maxU;
-			maxU = tmp;
-		}
-
-		if (renderer.renderMinX < 0.0D || renderer.renderMaxX > 1.0D)
-		{
-			minU = icon.getMinU();
-			maxU = icon.getMaxU();
-		}
-
-		if (renderer.renderMinY < 0.0D || renderer.renderMaxY > 1.0D)
-		{
-			maxV = icon.getMinV();
-			minV = icon.getMaxV();
-		}
-
-		tmp = maxU;
-		double u = minU;
-		double V = maxV;
-		double v = minV;
-		//
-		//		if (renderer.uvRotateWest == 1)
-		//		{
-		//			minU = icon.getInterpolatedU(renderer.renderMinY * 16.0D);
-		//			minV = icon.getInterpolatedV(16.0D - renderer.renderMinX * 16.0D);
-		//			maxU = icon.getInterpolatedU(renderer.renderMaxY * 16.0D);
-		//			maxV = icon.getInterpolatedV(16.0D - renderer.renderMaxX * 16.0D);
-		//			V = maxV;
-		//			v = minV;
-		//			tmp = minU;
-		//			u = maxU;
-		//			maxV = minV;
-		//			minV = V;
-		//		}
-		//		else if (renderer.uvRotateWest == 2)
-		//		{
-		//			minU = icon.getInterpolatedU(16.0D - renderer.renderMaxY * 16.0D);
-		//			maxV = icon.getInterpolatedV(renderer.renderMinX * 16.0D);
-		//			maxU = icon.getInterpolatedU(16.0D - renderer.renderMinY * 16.0D);
-		//			minV = icon.getInterpolatedV(renderer.renderMaxX * 16.0D);
-		//			tmp = maxU;
-		//			u = minU;
-		//			minU = maxU;
-		//			maxU = u;
-		//			V = minV;
-		//			v = maxV;
-		//		}
-		//		else if (renderer.uvRotateWest == 3)
-		//		{
-		//			minU = icon.getInterpolatedU(16.0D - renderer.renderMinX * 16.0D);
-		//			maxU = icon.getInterpolatedU(16.0D - renderer.renderMaxX * 16.0D);
-		//			maxV = icon.getInterpolatedV(renderer.renderMaxY * 16.0D);
-		//			minV = icon.getInterpolatedV(renderer.renderMinY * 16.0D);
-		//			tmp = maxU;
-		//			u = minU;
-		//			V = maxV;
-		//			v = minV;
-		//		}
 
 		double xx = x + renderer.renderMinX;
 		double XX = x + renderer.renderMaxX;
@@ -323,20 +193,20 @@ public class StuffDoorRenderer implements ISimpleBlockRenderingHandler
 			tessellator.addVertexWithUV(xx, YY, ZZ, minU, maxV);
 			tessellator.setColorOpaque_F(renderer.colorRedBottomLeft, renderer.colorGreenBottomLeft, renderer.colorBlueBottomLeft);
 			tessellator.setBrightness(renderer.brightnessBottomLeft);
-			tessellator.addVertexWithUV(xx, yy, ZZ, u, v);
+			tessellator.addVertexWithUV(xx, yy, ZZ, minU, minV);
 			tessellator.setColorOpaque_F(renderer.colorRedBottomRight, renderer.colorGreenBottomRight, renderer.colorBlueBottomRight);
 			tessellator.setBrightness(renderer.brightnessBottomRight);
 			tessellator.addVertexWithUV(XX, yy, ZZ, maxU, minV);
 			tessellator.setColorOpaque_F(renderer.colorRedTopRight, renderer.colorGreenTopRight, renderer.colorBlueTopRight);
 			tessellator.setBrightness(renderer.brightnessTopRight);
-			tessellator.addVertexWithUV(XX, YY, ZZ, tmp, V);
+			tessellator.addVertexWithUV(XX, YY, ZZ, maxU, maxV);
 		}
 		else
 		{
 			tessellator.addVertexWithUV(xx, YY, ZZ, minU, maxV);
-			tessellator.addVertexWithUV(xx, yy, ZZ, u, v);
+			tessellator.addVertexWithUV(xx, yy, ZZ, minU, minV);
 			tessellator.addVertexWithUV(XX, yy, ZZ, maxU, minV);
-			tessellator.addVertexWithUV(XX, YY, ZZ, tmp, V);
+			tessellator.addVertexWithUV(XX, YY, ZZ, maxU, maxV);
 		}
 	}
 
@@ -344,78 +214,10 @@ public class StuffDoorRenderer implements ISimpleBlockRenderingHandler
 	{
 		Tessellator tessellator = Tessellator.instance;
 
-		if (renderer.hasOverrideBlockTexture())
-		{
-			icon = renderer.overrideBlockTexture;
-		}
-
 		double minU = icon.getInterpolatedU(renderer.renderMinZ * 16.0D);
 		double maxU = icon.getInterpolatedU(renderer.renderMaxZ * 16.0D);
 		double maxV = icon.getInterpolatedV(16.0D - renderer.renderMaxY * 16.0D);
 		double minV = icon.getInterpolatedV(16.0D - renderer.renderMinY * 16.0D);
-		double tmp;
-
-		if (renderer.flipTexture)
-		{
-			tmp = minU;
-			minU = maxU;
-			maxU = tmp;
-		}
-
-		if (renderer.renderMinZ < 0.0D || renderer.renderMaxZ > 1.0D)
-		{
-			minU = icon.getMinU();
-			maxU = icon.getMaxU();
-		}
-
-		if (renderer.renderMinY < 0.0D || renderer.renderMaxY > 1.0D)
-		{
-			maxV = icon.getMinV();
-			minV = icon.getMaxV();
-		}
-
-		tmp = maxU;
-		double uu = minU;
-		double VV = maxV;
-		double vv = minV;
-
-		//		if (renderer.uvRotateNorth == 1)
-		//		{
-		//			minU = icon.getInterpolatedU(renderer.renderMinY * 16.0D);
-		//			maxV = icon.getInterpolatedV(16.0D - renderer.renderMaxZ * 16.0D);
-		//			maxU = icon.getInterpolatedU(renderer.renderMaxY * 16.0D);
-		//			minV = icon.getInterpolatedV(16.0D - renderer.renderMinZ * 16.0D);
-		//			VV = maxV;
-		//			vv = minV;
-		//			tmp = minU;
-		//			uu = maxU;
-		//			maxV = minV;
-		//			minV = VV;
-		//		}
-		//		else if (renderer.uvRotateNorth == 2)
-		//		{
-		//			minU = icon.getInterpolatedU(16.0D - renderer.renderMaxY * 16.0D);
-		//			maxV = icon.getInterpolatedV(renderer.renderMinZ * 16.0D);
-		//			maxU = icon.getInterpolatedU(16.0D - renderer.renderMinY * 16.0D);
-		//			minV = icon.getInterpolatedV(renderer.renderMaxZ * 16.0D);
-		//			tmp = maxU;
-		//			uu = minU;
-		//			minU = maxU;
-		//			maxU = uu;
-		//			VV = minV;
-		//			vv = maxV;
-		//		}
-		//		else if (renderer.uvRotateNorth == 3)
-		//		{
-		//			minU = icon.getInterpolatedU(16.0D - renderer.renderMinZ * 16.0D);
-		//			maxU = icon.getInterpolatedU(16.0D - renderer.renderMaxZ * 16.0D);
-		//			maxV = icon.getInterpolatedV(renderer.renderMaxY * 16.0D);
-		//			minV = icon.getInterpolatedV(renderer.renderMinY * 16.0D);
-		//			tmp = maxU;
-		//			uu = minU;
-		//			VV = maxV;
-		//			vv = minV;
-		//		}
 
 		double xx = x + renderer.renderMinX;
 		double yy = y + renderer.renderMinY;
@@ -427,22 +229,22 @@ public class StuffDoorRenderer implements ISimpleBlockRenderingHandler
 		{
 			tessellator.setColorOpaque_F(renderer.colorRedTopLeft, renderer.colorGreenTopLeft, renderer.colorBlueTopLeft);
 			tessellator.setBrightness(renderer.brightnessTopLeft);
-			tessellator.addVertexWithUV(xx, YY, ZZ, tmp, VV);
+			tessellator.addVertexWithUV(xx, YY, ZZ, maxU, maxV);
 			tessellator.setColorOpaque_F(renderer.colorRedBottomLeft, renderer.colorGreenBottomLeft, renderer.colorBlueBottomLeft);
 			tessellator.setBrightness(renderer.brightnessBottomLeft);
 			tessellator.addVertexWithUV(xx, YY, zz, minU, maxV);
 			tessellator.setColorOpaque_F(renderer.colorRedBottomRight, renderer.colorGreenBottomRight, renderer.colorBlueBottomRight);
 			tessellator.setBrightness(renderer.brightnessBottomRight);
-			tessellator.addVertexWithUV(xx, yy, zz, uu, vv);
+			tessellator.addVertexWithUV(xx, yy, zz, minU, minV);
 			tessellator.setColorOpaque_F(renderer.colorRedTopRight, renderer.colorGreenTopRight, renderer.colorBlueTopRight);
 			tessellator.setBrightness(renderer.brightnessTopRight);
 			tessellator.addVertexWithUV(xx, yy, ZZ, maxU, minV);
 		}
 		else
 		{
-			tessellator.addVertexWithUV(xx, YY, ZZ, tmp, VV);
+			tessellator.addVertexWithUV(xx, YY, ZZ, maxU, maxV);
 			tessellator.addVertexWithUV(xx, YY, zz, minU, maxV);
-			tessellator.addVertexWithUV(xx, yy, zz, uu, vv);
+			tessellator.addVertexWithUV(xx, yy, zz, minU, minV);
 			tessellator.addVertexWithUV(xx, yy, ZZ, maxU, minV);
 		}
 	}

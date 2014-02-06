ea4382125a08afda479ce98f133abddc11e9d468
Timothy McPherson <blarghedy@gmail.com>
2014-02-05 21:08:27 -0500

    AE Stuff now loads only if AE2 is installed
    Technically it loads no matter what, but it doesn't add or do anything
    if AE2 is not loaded.

--- src/main/java/stuffstuff/aestuff/AEStuff.java
+++ src/main/java/stuffstuff/aestuff/AEStuff.java
@@ -9,10 +9,9 @@ import stuffstuff.aestuff.info.ModInfo;
 import stuffstuff.aestuff.items.ItemsAEStuff;
 import stuffstuff.aestuff.proxy.CommonProxy;
 import stuffstuff.stuffstuff.StuffStuff;
-import stuffstuff.stuffstuff.blocks.Blocks;
 import stuffstuff.stuffstuff.client.interfaces.GuiHandler;
-import stuffstuff.stuffstuff.items.Items;
 import stuffstuff.stuffstuff.network.PacketHandler;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -22,11 +21,21 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION, dependencies = "after:stuffstuff;after:appeng")
+@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION, dependencies = "required-after:stuffstuff;after:appliedenergistics2")
 @NetworkMod(channels = { ModInfo.CHANNEL }, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class AEStuff
 {
-	public static CreativeTabs tabAEStuff = new TabAEStuff("AE Stuff");
+	public static CreativeTabs tabAEStuff;
+	private static boolean isAELoaded;
+
+	static 
+	{
+		isAELoaded = Loader.isModLoaded("appliedenergistics2");
+		if (isAELoaded)
+		{
+			tabAEStuff = new TabAEStuff("AE Stuff");
+		}
+	}
 
 	@Instance(ModInfo.ID)
 	public static AEStuff instance;
@@ -37,32 +46,41 @@ public class AEStuff
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e)
 	{
-		ConfigHandler.init(StuffStuff.stuffstuffConfig);
-		FluidsAEStuff.init();
-		ItemsAEStuff.init();
-		BlocksAEStuff.init();
+		if (isAELoaded)
+		{
+			ConfigHandler.init(StuffStuff.stuffstuffConfig);
+			FluidsAEStuff.init();
+			ItemsAEStuff.init();
+			BlocksAEStuff.init();
 
-		proxy.initSounds();
-		proxy.initRenderer();
-		proxy.registerHandlers();
+			proxy.initSounds();
+			proxy.initRenderer();
+			proxy.registerHandlers();
 
-		ItemsAEStuff.addNames();
-		BlocksAEStuff.addNames();
-		FluidsAEStuff.addNames();
+			ItemsAEStuff.addNames();
+			BlocksAEStuff.addNames();
+			FluidsAEStuff.addNames();
+		}
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent e)
 	{
-		ItemsAEStuff.registerRecipes();
-		BlocksAEStuff.registerTileEntities();
-		new GuiHandler();
+		if (isAELoaded)
+		{
+			ItemsAEStuff.registerRecipes();
+			BlocksAEStuff.registerTileEntities();
+			new GuiHandler();
+		}
 	}
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent e)
 	{
+		if (isAELoaded)
+		{
 
+		}
 	}
 
 }

09757e6a33285a229c02335fdaf153d1df515c51
Timothy McPherson <blarghedy@gmail.com>
2014-04-13 11:35:35 -0400

    More treegen infrastructure
    This should be a fairly easy to use collection of treegens.  Ideally,
    I'll end up with several static instances of this class generated
    sometime during initialization, one per biome or treegen type.

--- src/main/java/stuffstuff/stuffstuff/worldgen/trees/StuffTreeGenCollection.java
+++ src/main/java/stuffstuff/stuffstuff/worldgen/trees/StuffTreeGenCollection.java
@@ -2,6 +2,7 @@ package stuffstuff.stuffstuff.worldgen.trees;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -10,33 +11,33 @@ import com.google.gson.JsonObject;
 public class StuffTreeGenCollection
 {
 	private ArrayList<StuffTreeGen> treegens;
-	
+
 	public StuffTreeGenCollection()
 	{
 		treegens = new ArrayList<StuffTreeGen>();
 	}
-	
+
+	public StuffTreeGenCollection(JsonArray array)
+	{
+		addTreeGens(array);
+	}
+
 	public StuffTreeGenCollection(Collection<StuffTreeGen> gens)
 	{
 		treegens.addAll(gens);
 	}
-	
-	public StuffTreeGenCollection(JsonArray array)
-	{
-		
-	}
-	
+
 	public void addTreeGen(StuffTreeGen treegen)
 	{
 		treegens.add(treegen);
 	}
-	
+
 	public void addTreeGen(JsonObject treegen)
 	{
 		StuffTreeGen gen = new StuffTreeGen(treegen);
 		addTreeGen(gen);
 	}
-	
+
 	public void addTreeGens(JsonArray gens)
 	{
 		for (JsonElement element : gens)
@@ -44,9 +45,25 @@ public class StuffTreeGenCollection
 			addTreeGen(element.getAsJsonObject());
 		}
 	}
-	
+
 	public void addTreeGens(Collection<StuffTreeGen> gens)
 	{
-		
+		treegens.addAll(gens);
+	}
+
+	public StuffTreeGen getRandomGen(Random rand)
+	{
+		int index = rand.nextInt(treegens.size());
+		return treegens.get(index);
+	}
+
+	public int size()
+	{
+		return treegens.size();
+	}
+
+	public boolean removeTreeGen(StuffTreeGen gen)
+	{
+		return treegens.remove(gen);
 	}
 }
